cc6561025aa3a214816ddcfbce4ac4e54bc8a67e
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-03-27 22:15:44 +0000

    GetAll when search!= null tesy

--- tests/BusinessController.Tests.cs
+++ tests/BusinessController.Tests.cs
@@ -4,7 +4,6 @@ using Moq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-
 namespace tests
 {
     public class BusinessControllerTests
@@ -21,6 +20,19 @@ namespace tests
         }
 
         [Fact]
+        public async void AllMethods_WhenRepoThrowsException_ReturnsBadRequest(string method)
+        {
+            //Is there a way to call all methods in one test??
+
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Equal(404, resultObj.StatusCode); ;
+
+        }
+        [Fact]
         public async void GetAll_ReturnsOkObjectWhenSearchIsNull()
         {
             //Arrange
@@ -33,5 +45,40 @@ namespace tests
             Assert.NotNull(resultObj);
             Assert.Equal(200, resultObj.StatusCode);
         }
+
+        [Fact]
+        public async void GetAll_WhenSearchIsNullAndRepoThrowsException_ReturnsNotFound()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.GetAll()).Throws(new InvalidOperationException());
+
+            //Act
+            var result = await controller.GetAll();
+            var resultObj = result as NotFoundObjectResult;
+
+            //Assert
+            Assert.Equal(404, resultObj.StatusCode); ;
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("Dave")]
+        public async void GetAll_CallsSearchMethodOnMockRepoWhenSearchIsNotNull_ReturnsOkObject(string search)
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.Search(search));
+
+            //Act
+            var result = await controller.GetAll(search);
+            var resultObj = result as OkObjectResult;
+
+            //Asset
+            mockRepo.Verify(repo => repo.Search(search), Times.Once);
+
+            Assert.NotNull(resultObj);
+            Assert.Equal(200, resultObj.StatusCode);
+        }
+
     }
 }
