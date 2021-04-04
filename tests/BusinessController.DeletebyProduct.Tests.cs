774441014ac3c313cf17bfbac7bcd1afce5d1382
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-04-04 19:44:41 +0100

    DeletebyProduct tests

--- tests/BusinessController.DeletebyProduct.Tests.cs
+++ tests/BusinessController.DeletebyProduct.Tests.cs
@@ -19,6 +19,40 @@ namespace tests
             controller = new BusinessController(mockRepo.Object);
         }
 
-       
+        [Fact]
+        public void DeletebyProduct_CallsDeleteMethodbyProductOnMockRepoWithCorrectId_ReturnsOkObject()
+        {
+            //Arrange
+            long expectedId = 12;
+
+            mockRepo.Setup(repo => repo.DeletebyProduct(expectedId));
+
+            //Act
+            var result = controller.DeletebyProduct(expectedId);
+            var resultObj = result as OkObjectResult;
+            var model = resultObj.Value as String;
+
+            //Assert
+            mockRepo.Verify(repo => repo.DeletebyProduct(expectedId), Times.Once);
+
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, resultObj.StatusCode);
+
+            Assert.Equal(model, $"Product at {expectedId} is deleted");
+        }
+
+        [Fact]
+        public void DeletebyProduct_WhenRepoThrowsException_ReturnsBadRequest()
+        {
+            //Arrange
+            long id = 5;
+            mockRepo.Setup(repo => repo.DeletebyProduct(id)).Throws(new Exception());
+
+            //Act
+            var result = controller.DeletebyProduct(id);
+
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
     }
 }
